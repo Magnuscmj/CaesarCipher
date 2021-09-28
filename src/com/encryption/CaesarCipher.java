d7254dcfd080e4e0a0e76b9aa128fa9b45430ab4
Magnuscmj <56219481+Magnuscmj@users.noreply.github.com>
2021-09-28 11:08:35 +0200

    created the base project

---
+++ src/com/encryption/CaesarCipher.java
@@ -0,0 +1,16 @@
+package com.encryption;
+
+public class CaesarCipher {
+
+    public static String encoding() {
+
+    }
+
+    public static String decoding() {
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
