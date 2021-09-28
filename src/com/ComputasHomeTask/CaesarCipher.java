c156eb3f6e07d5757b1f2dfe95598440083494f9
Magnuscmj <56219481+Magnuscmj@users.noreply.github.com>
2021-09-28 11:29:24 +0200

    created the first logics for the encoding :)

---
+++ src/com/ComputasHomeTask/CaesarCipher.java
@@ -0,0 +1,23 @@
+package com.ComputasHomeTask;
+
+public class CaesarCipher {
+
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encoding(String plainText, int Key) {
+        plainText = plainText.toLowerCase();
+        String chiperText = " ";
+        for(int i = 0; i < plainText.length(); i++) {
+
+        }
+        return chiperText;
+    }
+
+    public static String decoding() {
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
